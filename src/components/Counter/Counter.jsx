dd8d44001cb3bcb17a23f5a4c1b5ec0d14261c2e
FarzeenZainab <farzeenzaidi51@gmail.com>
2024-02-26 16:59:25 +0500

    using the useCallback function

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -29,13 +29,13 @@ export default function Counter({ initialCount }) {
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
@@ -44,6 +44,20 @@ export default function Counter({ initialCount }) {
         <strong>is {initialCountIsPrime ? "a" : "not a"}</strong> prime number.
       </p>
       <p>
+        {/* 
+           // ! Un-optimized code: 
+            In this implementation when the count updates Icon button component 
+            re renders every time. 
+
+            We added memo function to avoid unnecessary re-renders but, it is still re renders.
+            The reason in the props passed to the IconButton component changes  on each render specifically
+            the onClick prop sends new function on each re render AS THE FUNCTION BODY IS RECREATED DURING
+            EACH RERENDER 
+
+            How to fix it?
+            To memoize function body we use useCallback function, which will only recreate the function body if any
+            of the provided dependencies changes
+        */}
         <IconButton icon={MinusIcon} onClick={handleDecrement}>
           Decrement
         </IconButton>
